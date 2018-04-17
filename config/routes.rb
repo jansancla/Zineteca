75b1998fb99c26e747bd049b0945e0657e94d8c6
Jansancla <a.sanchez223@usp.ceu.es>
2018-04-17 18:27:19 +0000

    Creación de las rutas estáticas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# rutas estáticas home
+root 'static#home'
+get 'contact' => 'static#contact'
+get 'about_us' => 'static#about_us'
+get 'error' => 'static#error'
